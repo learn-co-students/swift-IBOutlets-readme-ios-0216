76b6085a939fc8c1e982943fa8edc67bd2ab146f
AdrianBrown1 <adrian.angel.brown@gmail.com>
2016-07-12 13:19:40 -0400

    swift outlet stuff

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        textDisplay.text = "Zzz..."
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        print("Hello world button tapped !")
+        textDisplay.text = "Hello World!"
+        
+    }
+    @IBAction func goodbyeWorld(_ sender: AnyObject) {
+        print("Goodbye button tapped")
+        textDisplay.text = "Goodbye World!"
+    }
+    
+    @IBAction func unicorn(_ sender: AnyObject) {
+        print("Unicorn Button tapped")
+        textDisplay.text = "x"
+    }
+    
+    
+    
+    
 }
